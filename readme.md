f112a5dbcf3f7972f280f531f3bf0cd9641c6d10
Ben Conolly <ben@thinkmill.com.au>
2019-06-15 18:27:33 +1000

    initial commit

---
+++ readme.md
@@ -0,0 +1,16 @@
+# 💯💜🎉
+
+This package generates unique identifiers, but the identifiers are emoji
+
+```javascript
+import eid from 'emoji-uid
+
+const id = eid()
+// id of 💯💜🎉 or similar
+```
+
+This library includes 858 emojis, or more than 631 million possible unique ids,
+
+Some emojis have been removed from the set for not working on my machine, implying negative sentiment, or my personal preference.
+
+This project inspired by human-readable-ids, but with more troll.
---
+++ 💖💙💜.js
@@ -0,0 +1,23 @@
+const emojis = require("./💯💯💯");
+
+// 11 lists
+// Emoji lists lengths [69, 66, 62, 116, 64, 96, 53, 118, 163, 16, 35];
+
+const getEmoji = () => {
+  let list = emojis[Math.floor(Math.random() * emojis.length)];
+
+  return list[Math.floor(Math.random() * list.length)];
+};
+
+const getEmojis = (number = 3) => {
+  let id = "";
+  while (number > 0) {
+    id = id + getEmoji();
+    number--;
+  }
+  return id;
+};
+
+console.log(getEmojis());
+
+module.exports = getEmojis;
